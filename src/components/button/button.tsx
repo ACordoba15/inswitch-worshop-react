b9b3cbb7d691bae2435dd89b0e1c999147d45fa3
andrescr <jcordoba@teledolar.com>
2024-11-01 11:35:32 -0600

    Ajuste en el llamado al post

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -1,52 +1,11 @@
-import { useState } from "react";
-import { axiosInstance, getAccessToken } from "../../utils/genericOps";
-import { walletId } from "../../utils/constants";
+
+import { AddPaymentMethod} from "../../utils/genericOps";
 
 export default function Button () {
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState();
-  
   const addPaymentMethod = async () => {
-    setLoading(true);
-    try {
-      const token = await getAccessToken();
-      const entityBalanceRequest = await axiosInstance.post(
-        `${import.meta.env.VITE_PUBLIC_API_WALLET}/${walletId}/paymentmethods`,
-        {
-          paymentMethodAlias: "My USD emoney account",
-          paymentMethodType: "emoney-usd",
-          paymentMethodStatus: "active",
-          inactiveReason: null,
-          paymentMethodData: {
-            ACCOUNT_ID: "13848",
-            maxBalance: "999999999",
-            overdraft: "0",
-          },
-          paymentMethodLinkData: [
-            {
-              key: "string",
-              value: "string",
-            },
-          ],
-          paymentMethodMetadata: [
-            {
-              key: "string",
-              value: "string",
-            },
-          ],
-        },
-        {
-          headers: {
-            "X-User-Bearer": `Bearer ${token}`,
-          },
-        }
-      );
-    } catch (err: any) {
-      setError(err);
-    } finally {
-      setLoading(false);
-    }
+    const addPaymentMethod = await AddPaymentMethod()
   };
+
   return (
     <button onClick={addPaymentMethod}>
       Agregar metodo de pago
